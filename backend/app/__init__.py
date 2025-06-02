d96f02d021b89edd59fc0659ec2fb02843ae6d26
syrus2008 <thibaut.dieu@gmail.com>
2025-06-02 22:08:59 +0200

    Resolve merge conflicts in Procfile, app/__init__.py and requirements.txt

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,10 +7,3 @@ __all__ = ['api_router', 'Storage', 'AuthManager']
 
 # Initialiser le stockage
 storage = Storage()
-=======
-from api import api
-from storage import Storage
-from auth import AuthManager
-
-__all__ = ['api', 'Storage', 'AuthManager']
->>>>>>> 4b6f3fc372f3cc88130fc9a515a05ef70f4d3d6f
